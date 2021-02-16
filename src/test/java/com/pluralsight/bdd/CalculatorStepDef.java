801bc17e1ab2e9d9ceade12fff77e0de60a37f4a
Nafia Amaziane <nafia.amaziane.etudes@gmail.com>
2021-02-16 02:27:19 +0100

    regular expression vs cucumber expression

--- src/test/java/com/pluralsight/bdd/CalculatorStepDef.java
+++ src/test/java/com/pluralsight/bdd/CalculatorStepDef.java
@@ -4,6 +4,8 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorStepDef {
@@ -21,13 +23,22 @@ public class CalculatorStepDef {
         result = calculator.add(double1, double2);
     }
 
-    @When("I substract {double} from {double}")
+    @When("^I substract (.*) from (.*)$")
     public void i_substract_from(double double1, double double2) {
         //result = 0.0;
         result = calculator.substract(double1, double2);
     }
 
-    @When("I divide {double} from {double}")
+
+    @When("^s?he has some great an? hisbend and s?he matches all to things how hase s?he$")
+    public void testing(String double1, String double2,String douxble2) {
+        result = 0.0;
+    }
+    @When("^s?he (?:orders|has ordered) (\\d) (.*) smothy$")
+    public void testing2(String double1, Integer double2, String douxble2) {
+        result = 0.0;
+    }
+    @When("^I divide {double} from {double}")
     public void i_divide_from(double double1, double double2) {
         // Write code here that turns the phrase above doubleo concrete actions
         //result = 0.0;
--- src/test/resources/com/pluralsight/bdd/calculator.feature
+++ src/test/resources/com/pluralsight/bdd/calculator.feature
@@ -26,3 +26,16 @@ Feature: calcular
     When I divide 100.0 from 4.0
     Then I should have 25.0
 
+  Scenario: Substracting two numbers
+    Given a have a calculator
+    When I substract 11.0 from 9.0
+    Then I should have 2.0
+
+  Scenario: Substracting two numbers
+    Given a have a calculator
+    When she has some great a hisbend and he matches all to things how hase he
+    And he has some great an hisbend and she matches all to things how hase she
+    And she orders 2 GreenGoodsness smothy
+    And he orders 2 GreenGoodsness smothy
+    And she has ordered 2 GreenGoodsness smothy
+    And he has ordered 2 GreenGoodsness smothy
